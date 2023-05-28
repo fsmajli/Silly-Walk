050407b1d277f8f51c70085837f997f524d18542
Fatbardh Smajli <43210697+fsmajli@users.noreply.github.com>
2023-05-28 13:10:10 +0200

    Delete monty directory

--- monty/GUI/classifyWalk.m
+++
@@ -1,8 +0,0 @@
-function YPred = classifyWalk(model, XTest)
-% This is a trivial example for a classifier. It classifies any input as a
-% normal walk.
-YPred = classify(model, XTest);
-%YPred = categorical(repmat({'Normal walk'}, size(XTest)));
-
-end
-
--- monty/GUI/trainSillyWalkClassifier.m
+++
@@ -1,9 +0,0 @@
-function [model,info] = trainSillyWalkClassifier(XTrain, YTrain)
-% For this trivial example, no model is required. 
-[layers, options] = specify_network();
-[model,info] = trainNetwork(XTrain, YTrain, layers, options);
-
-
-save(fullfile(fileparts(mfilename('fullpath')), 'Model.mat'), 'model'); % do not change this line
-end
-
